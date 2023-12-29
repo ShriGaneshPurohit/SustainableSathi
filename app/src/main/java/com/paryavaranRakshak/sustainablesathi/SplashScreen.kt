061acd420fab16526b0c623bfb19c2103ab2c1f5
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-29 17:03:49 +0530

    XML

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/SplashScreen.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/SplashScreen.kt
@@ -3,7 +3,7 @@ package com.paryavaranRakshak.sustainablesathi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
