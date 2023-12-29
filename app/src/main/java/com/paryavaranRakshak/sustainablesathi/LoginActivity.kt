5a653080986cbfbb598c9658af24c3ccd6ec416e
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-29 17:37:46 +0530

    Firebase Connected

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/LoginActivity.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/LoginActivity.kt
@@ -1,11 +1,99 @@
 package com.paryavaranRakshak.sustainablesathi
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.paryavaranRakshak.sustainablesathi.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding: ActivityLoginBinding
+
+    //login
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //login
+        auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //initialize progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Please wait...")
+        progressDialog.setCancelable(false)
+
+        //btn google
+        //binding.btnGoogle.setOnClickListener { signInGoogle() }
+
     }
+
+    private fun signInGoogle() {
+        progressDialog.show()
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                updateUI(account)
+            }
+        } else {
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                // Save onboarding status and login type in SharedPreferences
+                exit()
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                exit()
+            }
+        }
+    }
+
+    private fun exit() {
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
