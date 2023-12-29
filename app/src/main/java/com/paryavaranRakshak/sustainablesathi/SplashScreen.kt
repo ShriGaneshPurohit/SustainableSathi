6eab11b85cf64b5946ec62090695563a88a3648d
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-29 17:05:02 +0530

    XML

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/SplashScreen.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/SplashScreen.kt
@@ -1,11 +1,26 @@
 package com.paryavaranRakshak.sustainablesathi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
+
+    //splash time
+    private val Splash_Time_Out = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Splash Screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(R.anim.zoom_in, R.anim.static_animation)
+            finishAffinity()
+        }, Splash_Time_Out.toLong())
+
     }
 }
