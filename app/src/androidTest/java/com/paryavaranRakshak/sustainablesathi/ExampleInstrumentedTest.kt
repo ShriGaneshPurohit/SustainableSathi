680d009000731a101025f0a90ae6948c5e959281
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-29 16:43:33 +0530

    XML

--- app/src/androidTest/java/com/paryavaranRakshak/sustainablesathi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/paryavaranRakshak/sustainablesathi/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.sustainablesathi
+package com.paryavaranRakshak.sustainablesathi
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/test/java/com/paryavaranRakshak/sustainablesathi/ExampleUnitTest.kt
+++ app/src/test/java/com/paryavaranRakshak/sustainablesathi/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.sustainablesathi
+package com.paryavaranRakshak.sustainablesathi
 
 import org.junit.Test
 
