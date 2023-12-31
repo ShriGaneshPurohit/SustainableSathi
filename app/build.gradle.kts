49e0ebf8d712d08b2b8e6e21ffd8db38f3513654
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-31 21:43:08 +0530

    Seller Profile Creation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,7 @@ dependencies {
     //image blur
     implementation("jp.wasabeef:glide-transformations:4.3.0")
 
+    //image picker
+    implementation("com.github.dhaval2404:imagepicker:2.1")
+
 }
