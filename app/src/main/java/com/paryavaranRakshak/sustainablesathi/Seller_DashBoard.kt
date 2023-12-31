0928d3aac44b785edae8857fced31728db506873
ManavPatni <developer.mnvpatni@gmail.com>
2023-12-31 16:38:35 +0530

    Seller Profile Creation

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/Seller_DashBoard.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/Seller_DashBoard.kt
@@ -3,9 +3,9 @@ package com.paryavaranRakshak.sustainablesathi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Seller_DashBoard : AppCompatActivity() {
+class SellerDashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seller_dash_board)
+        setContentView(R.layout.activity_seller_dashboard)
     }
 }
