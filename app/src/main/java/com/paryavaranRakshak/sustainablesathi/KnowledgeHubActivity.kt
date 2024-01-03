eca9e16e26d4d19822f49d7e4f1786d13aa64bb5
ManavPatni <developer.mnvpatni@gmail.com>
2024-01-03 17:57:50 +0530

    Seller Profile Creation

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/KnowledgeHubActivity.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/KnowledgeHubActivity.kt
@@ -1,28 +1,40 @@
 package com.paryavaranRakshak.sustainablesathi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.paryavaranRakshak.sustainablesathi.databinding.ActivityKnowledgeHubBinding
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener
 
 class KnowledgeHubActivity : AppCompatActivity() {
 
     //view binding
     private lateinit var binding: ActivityKnowledgeHubBinding
 
-    //yt player
-    private lateinit var youTubePlayer: YouTubePlayer
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityKnowledgeHubBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.cvStage1.setOnClickListener{
+            val intent = Intent(this,YoutubePlayerActivity::class.java)
+            intent.putExtra("videoId","MQLadfsvfLo")
+            startActivity(intent)
+        }
+        binding.cvStage2.setOnClickListener{
+            val intent = Intent(this,YoutubePlayerActivity::class.java)
+            intent.putExtra("videoId","h5Z5m5by9UA")
+            startActivity(intent)
+        }
+        binding.cvStage3.setOnClickListener{
+            val intent = Intent(this,YoutubePlayerActivity::class.java)
+            intent.putExtra("videoId","ApdkhWd7SfQ")
+            startActivity(intent)
+        }
+        binding.cvStage4.setOnClickListener{
+            val intent = Intent(this,YoutubePlayerActivity::class.java)
+            intent.putExtra("videoId","-uyIzKIw0xY")
+            startActivity(intent)
+        }
 
     }
 
--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/YoutubePlayerActivity.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/YoutubePlayerActivity.kt
@@ -2,10 +2,10 @@ package com.paryavaranRakshak.sustainablesathi
 
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.paryavaranRakshak.sustainablesathi.databinding.ActivityYoutubePlayerBinding
@@ -17,21 +17,17 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrC
 
 class YoutubePlayerActivity : AppCompatActivity() {
 
-    //view binding
-    private lateinit var binding:  ActivityYoutubePlayerBinding
+    // View binding
+    private lateinit var binding: ActivityYoutubePlayerBinding
 
     private lateinit var youTubePlayer: YouTubePlayer
     private var isFullScreen = false
 
     private var videoId: String? = null
 
-    private val onBackPressedCallback = object : OnBackPressedCallback(true){
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
-            if (isFullScreen){
-                youTubePlayer.toggleFullscreen()
-            }else{
-                finish()
-            }
+            finish()
         }
     }
 
@@ -39,26 +35,28 @@ class YoutubePlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityYoutubePlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //back button
+        // Back button
         onBackPressedDispatcher.addCallback(onBackPressedCallback)
+
+        videoId = intent.getStringExtra("videoId").toString()
 
         lifecycle.addObserver(binding.ytPlayer)
 
-        binding.ytPlayer.addFullscreenListener(object : FullscreenListener{
+        binding.ytPlayer.addFullscreenListener(object : FullscreenListener {
             override fun onEnterFullscreen(fullscreenView: View, exitFullscreen: () -> Unit) {
                 isFullScreen = true
                 binding.container.visibility = View.VISIBLE
                 binding.container.addView(fullscreenView)
 
-                WindowInsetsControllerCompat(window!!,binding.rootView).apply {
+                WindowInsetsControllerCompat(window, binding.rootView).apply {
                     hide(WindowInsetsCompat.Type.systemBars())
-                    systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                    systemBarsBehavior =
+                        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                 }
 
-                if (requestedOrientation != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+                if (requestedOrientation != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
                     requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
                 }
-
             }
 
             override fun onExitFullscreen() {
@@ -66,48 +64,40 @@ class YoutubePlayerActivity : AppCompatActivity() {
                 binding.container.visibility = View.GONE
                 binding.container.removeAllViews()
 
-                WindowInsetsControllerCompat(window!!,binding.rootView).apply {
+                WindowInsetsControllerCompat(window, binding.rootView).apply {
                     show(WindowInsetsCompat.Type.systemBars())
                 }
 
-                if (requestedOrientation != ActivityInfo.SCREEN_ORIENTATION_SENSOR){
+                if (requestedOrientation != ActivityInfo.SCREEN_ORIENTATION_SENSOR) {
                     requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
                 }
-
             }
-
         })
 
-        val youtubePlayerListener = object : AbstractYouTubePlayerListener(){
+        val youtubePlayerListener = object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 this@YoutubePlayerActivity.youTubePlayer = youTubePlayer
-                youTubePlayer.loadOrCueVideo(lifecycle,videoId!!,0f)
+                youTubePlayer.loadOrCueVideo(lifecycle, videoId!!, 0f)
             }
         }
 
-        val  iFramePlayerOptions = IFramePlayerOptions.Builder()
+        val iFramePlayerOptions = IFramePlayerOptions.Builder()
             .controls(1)
             .fullscreen(1)
             .autoplay(1)
             .build()
 
         binding.ytPlayer.enableAutomaticInitialization = false
-        binding.ytPlayer.initialize(youtubePlayerListener,iFramePlayerOptions)
-        youTubePlayer.toggleFullscreen()
+        binding.ytPlayer.initialize(youtubePlayerListener, iFramePlayerOptions)
 
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            if (!isFullScreen){
-                youTubePlayer.toggleFullscreen()
-            }
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            if (!isFullScreen){
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE || newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (!isFullScreen) {
                 youTubePlayer.toggleFullscreen()
             }
         }
     }
-
 }
