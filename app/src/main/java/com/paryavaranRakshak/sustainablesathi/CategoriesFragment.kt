42a7e786a76d3e3908d30243d24cd5dd8eec15b5
ManavPatni <developer.mnvpatni@gmail.com>
2024-01-31 21:35:53 +0530

    Categories layout fixed

--- app/src/main/java/com/paryavaranRakshak/sustainablesathi/CategoriesFragment.kt
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/CategoriesFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CatagoriesFragment.newInstance] factory method to
+ * Use the [CategoriesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CatagoriesFragment : Fragment() {
+class CategoriesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class CatagoriesFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CatagoriesFragment().apply {
+            CategoriesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
