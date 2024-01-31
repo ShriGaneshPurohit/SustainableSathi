9fb2657561d26e7d86198f9117ffef0a1a838e48
ShreeGaneshPurohit <89515358+ShreeGaneshPurohit@users.noreply.github.com>
2024-01-31 19:34:12 +0530

    activity_knowledge_hub.xml Updated with ui

---
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/other/BuyersCategoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.paryavaranRakshak.sustainablesathi.other
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paryavaranRakshak.sustainablesathi.R
+
+class BuyersCategoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buyers_categories)
+    }
+}
