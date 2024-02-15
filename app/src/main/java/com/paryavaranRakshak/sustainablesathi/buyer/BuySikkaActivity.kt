0d04b550978d33a4220ff08521e50c60b69e67cb
ManavPatni <developer.mnvpatni@gmail.com>
2024-02-15 14:35:57 +0530

    Testing

---
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/buyer/BuySikkaActivity.kt
@@ -0,0 +1,12 @@
+package com.paryavaranRakshak.sustainablesathi.buyer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paryavaranRakshak.sustainablesathi.R
+
+class BuySikkaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_sikka)
+    }
+}
