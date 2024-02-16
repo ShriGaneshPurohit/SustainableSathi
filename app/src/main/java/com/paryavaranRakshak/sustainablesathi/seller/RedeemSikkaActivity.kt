35222403b048d3861f92979c947a20a5fb8146b9
ShreeGaneshPurohit <89515358+ShreeGaneshPurohit@users.noreply.github.com>
2024-02-16 11:01:20 +0530

    Added Sikka

---
+++ app/src/main/java/com/paryavaranRakshak/sustainablesathi/seller/RedeemSikkaActivity.kt
@@ -0,0 +1,12 @@
+package com.paryavaranRakshak.sustainablesathi.seller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.paryavaranRakshak.sustainablesathi.R
+
+class RedeemSikkaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redeem_sikka)
+    }
+}
